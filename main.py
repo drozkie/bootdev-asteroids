a4e6eb1baceeaa964578d66671c84c5a54ca2de9
drozkie <123610207+drozkie@users.noreply.github.com>
2024-12-20 00:42:16 -0500

    Added rotation, movement, asteroids, and collision

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import pygame
+from asteroid import *
 from constants import *
-
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import *
+from asteroidfield import *
 
 def main():
     print("Starting asteroids!")
+
+    pygame.init()
+
+    #CREATE GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    #CONFIGURE GROUPS
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    #SET VARIABLES
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #GROUP (UPDATABLE, DRAWABLE)
+    field = AsteroidField()
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
@@ -16,6 +35,20 @@ def main():
                 return
 
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+
+        dt = game_clock.tick(60) / 1000
+
+        for object in updatable:
+            object.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
+        for asteroid in asteroids:
+            if p.check_collision(asteroid):
+                print("Game over!")
+                return
+
         pygame.display.flip()
 
 if __name__ == "__main__":
